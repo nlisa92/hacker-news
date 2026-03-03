b4bb9caeaf028df5f808153eb44e7a32b4a60152
nlisa92 <irina.zamula.92@gmail.com>
2026-03-03 12:55:35 +0300

    feat: add localStorage persistence to Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,47 @@ import { configureStore } from "@reduxjs/toolkit";
 import commentsReducer from "./slices/commentsSlice";
 import newsReducer from "./slices/newsSlice";
 
+export const loadNews = () => {
+  try {
+    const data = localStorage.getItem("news");
+    return data ? JSON.parse(data) : undefined;
+  } catch {
+    return undefined;
+  }
+};
+
+export const loadComments = () => {
+  try {
+    const data = localStorage.getItem("comments");
+    return data ? JSON.parse(data) : undefined;
+  } catch {
+    return undefined;
+  }
+};
+
+const saveStateMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+
+  const { news, comments } = store.getState();
+
+  const limitedComments = Object.fromEntries(
+    Object.entries(comments)
+      .slice(-500)
+      .map(([id, c]) => [id, { id: c.id, text: c.text, kids: c.kids }]),
+  );
+  localStorage.setItem("news", JSON.stringify(news));
+  localStorage.setItem("comments", JSON.stringify(limitedComments));
+  return result;
+};
+
 export const store = configureStore({
-    reducer: {
-        news: newsReducer,
-        comments: commentsReducer,
-    }
-})
+  reducer: {
+    news: newsReducer,
+    comments: commentsReducer,
+  },
+  preloadedState: {
+    news: loadNews(),
+    comments: loadComments(),
+  },
+  middleware: (getDefault) => getDefault().concat(saveStateMiddleware),
+});
